d81c68217ff9fc241c184bbb7b3e39ab1daba1ca
Yuta Matsuzawa <matsuzawa@theoc.kuchem.kyoto-u.ac.jp>
2019-11-11 15:57:49 +0900

    first commit

---
+++ freqerica/core.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from . import helpers
+import numpy
+
+def get_hmm():
+    """Get a thought."""
+    return 'hmmm...'
+
+
+def hmm():
+    """Contemplation..."""
+    if helpers.get_answer():
+        print(get_hmm())
