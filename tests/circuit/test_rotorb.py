71a773057e3eb4c4bd83ec30ce6a95633ff3948a
Yuta Matsuzawa <matsuzawa@theoc.kuchem.kyoto-u.ac.jp>
2019-11-14 16:43:25 +0900

    [modify] testを追加

--- tests/circuit/test_rotorb.py
+++ tests/circuit/test_rotorb.py
@@ -4,13 +4,112 @@ import unittest
 import freqerica.circuit.rotorb
 
 
+def get_random_antihermite_mat(N):
+    import numpy as np
+    m  = np.random.rand(N*N).reshape(N,N)*1j
+    m += np.random.rand(N*N).reshape(N,N)
+    for i in range(N):
+        m[i,i] = m[i,i].imag * 1j
+        for j in range(N):
+            m[i,j] = -np.conjugate(m[j,i])
+    return m
+
+
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
-    def test_absolute_truth_and_meaning(self):
-        f = freqerica.operator.ham.construct_exact_ham
-        assert True
+    def test_decomp_unitary(self):
+        import numpy as np
+        import scipy.linalg
+        from pprint import pprint
+        np.set_printoptions(linewidth=300)
+        np.random.seed(100)
 
+        def single_test():
+            N=30
+            #umat = get_random_umat(N)
+            k = get_random_antihermite_mat(N)
+            umat = scipy.linalg.expm(k)
+            steps, phases_diag = freqerica.circuit.rotorb.decomp_unitary(umat)
+            #pprint(steps)
 
+            operations = []
+            for step in steps: operations += list(step)
+            mats = freqerica.circuit.rotorb.get_rotation_matrices(N, operations)
+
+            #print('kappa\n', k, sep='')
+            #print('umat\n', umat, sep='')
+            #print('dot(U,U.T)\n', umat.dot(np.conjugate(umat.T)),sep='')
+            for m in mats:
+                umat = np.dot(m, umat)
+            #print('r...ru\n', umat, sep='')
+            #print('exp(phase)\n', np.exp(1j*phases_diag), sep='')
+            diff = np.exp(1j*phases_diag)-np.diag(umat)
+            #print('diff(phase)\n', diff, sep='')
+            
+            print('[trial{:02d}] maxdiff : {:.1e}'.format(i,max(np.abs(diff))))
+            self.assertLess(max(np.abs(diff)), 1e-10)
+
+        print()
+        for i in range(10):
+            with self.subTest(i=i):
+                single_test()
+
+    def test_rotorb_circuit(self):
+        import numpy as np
+        import scipy.linalg
+        np.set_printoptions(linewidth=300)
+
+        def single_test(i):
+            N = 8
+
+            #k = np.random.rand(N,N)
+            #k -= (k+k.T)/2  # random real antisymmetric matrix
+            k = get_random_antihermite_mat(N)
+            umat = scipy.linalg.expm(k)
+            #print(umat)
+
+            c1 = freqerica.circuit.rotorb.OrbitalRotation(umat)
+
+            from openfermion import FermionOperator, jordan_wigner, get_sparse_operator
+            fop = FermionOperator()
+            for p in range(N):
+                for q in range(N):
+                    fop += FermionOperator( ((p,1),(q,0)), k[p,q] )
+            qop = jordan_wigner(fop)
+            #print(qop)
+
+            #from freqerica.circuit.trotter import TrotterStep
+            from freqerica.util.qulacsnize import convert_state_vector
+            import qulacs
+            #M = 100
+            #c2 = TrotterStep(N, qop/M)
+
+            s1 = qulacs.QuantumState(N)
+            s1.set_Haar_random_state()
+            #s2 = s1.copy()
+            s3 = convert_state_vector(N, s1.get_vector())
+
+            c1._circuit.update_quantum_state(s1)
+            #for i in range(M): c2._circuit.update_quantum_state(s2)
+
+            s3 = scipy.sparse.linalg.expm_multiply(get_sparse_operator(qop), s3)
+
+            #ip12 = qulacs.state.inner_product(s1, s2)
+            ip13 = np.conjugate(convert_state_vector(N, s1.get_vector())).dot(s3)
+            #ip23 = np.conjugate(convert_state_vector(N, s2.get_vector())).dot(s3)
+            #print('dot(s1,s2)', abs(ip12), ip12)
+            #print('dot(s1,s3)', abs(ip13), ip13)
+            #print('dot(s2,s3)', abs(ip23), ip23)
+
+            print('[trial{:02d}] fidelity-1 : {:+.1e}   dot=({:+.5f})'.format(i, abs(ip13)-1, ip13))
+
+        print()
+        for i in range(10):
+            with self.subTest(i=i):
+                single_test(i)
+
+            
+                
 if __name__ == '__main__':
     unittest.main()
--- tests/circuit/test_trotter.py
+++ tests/circuit/test_trotter.py
@@ -7,10 +7,99 @@ import freqerica.circuit.trotter
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
-    def test_absolute_truth_and_meaning(self):
-        f = freqerica.operator.ham.construct_exact_ham
-        assert True
+    def test_trotterstep_circuit(self):
+        from freqerica.operator.symbol import WrappedExpr as Symbol
+        from openfermion import FermionOperator, jordan_wigner, get_sparse_operator
+        from sympy import Array
+        import numpy as np
+        np.random.seed(100)
+        import scipy
+        import qulacs
 
+        n_orb = 4#2
+
+        const = Symbol('const')
+        T1 = [[None for _ in range(n_orb)] for _ in range(n_orb)]
+        for p in range(n_orb):
+            for q in range(p, n_orb):
+                t = Symbol('t{}{}'.format(p,q))
+                T1[p][q] = T1[q][p] = t
+        T1 = Array(T1)
+        print(T1)
+
+        const_value = np.random.rand()
+        T1_value = np.random.rand(n_orb,n_orb)*0.01
+        T1_value += T1_value.T
+        print(const_value)
+        print(T1_value)
+
+        def op1e(const, Tmat):
+            fop = FermionOperator('', const)
+            for p in range(n_orb):
+                for q in range(n_orb):
+                    fop += FermionOperator( ((2*p  , 1),(2*q  , 0)), Tmat[p,q] )
+                    fop += FermionOperator( ((2*p+1, 1),(2*q+1, 0)), Tmat[p,q] )
+            return fop
+
+        fop_symbol = op1e(const, T1)
+        qop_symbol = jordan_wigner(fop_symbol)
+        print(fop_symbol)
+        print(qop_symbol)
+
+        n_qubit = n_orb*2
+
+        # c1 : TrotterStep with symbolic qop
+        c1 = freqerica.circuit.trotter.TrotterStep(n_qubit, (-1j)*qop_symbol)
+        print(c1)
+        symbol_number_pairs = [(const, const_value), (T1, T1_value)]
+        c1.subs(symbol_number_pairs)
+        print(c1)
+
+        # c2 : TrotterStep with numerical qop
+        fop_number = op1e(const_value, T1_value)
+        qop_number = jordan_wigner(fop_number)
+        c2 = freqerica.circuit.trotter.TrotterStep(n_qubit, (-1j)*qop_number)
+        print(c2)
+
+        # c3 : oldtype with numerical qop
+        c3 = qulacs.QuantumCircuit(n_qubit)
+        freqerica.circuit.trotter.trotter_step_2nd_order(c3, (-1j)*qop_number)
+
+
+
+        s0 = qulacs.QuantumState(n_qubit)
+        s0.set_Haar_random_state()
+        s1 = s0.copy()
+        s2 = s0.copy()
+        s3 = s0.copy()
+        from freqerica.util.qulacsnize import convert_state_vector
+        sv = convert_state_vector( n_qubit, s0.get_vector() )
+
+        corr1 = []
+        corr2 = []
+        corr3 = []
+        corrv = []
+        for t in range(100):
+            corr1.append( qulacs.state.inner_product(s0, s1) )
+            corr2.append( qulacs.state.inner_product(s0, s2) )
+            corr3.append( qulacs.state.inner_product(s0, s3)*np.exp(-1j*qop_number.terms[()]*t) )
+            corrv.append( np.dot(np.conjugate(convert_state_vector(n_qubit, s0.get_vector())), sv) )
+            c1._circuit.update_quantum_state(s1)
+            c2._circuit.update_quantum_state(s2)
+            c3.update_quantum_state(s3)
+            sv = scipy.sparse.linalg.expm_multiply(-1j * get_sparse_operator(qop_number), sv)
+
+        #import matplotlib.pyplot as plt
+        #plt.plot(np.array(corr1).real, label='1')
+        #plt.plot(np.array(corr2).real, label='2')
+        #plt.plot(np.array(corr3).real, label='3')
+        #plt.plot(np.array(corrv).real, label='4')
+        #plt.legend()
+        #plt.show()
+
+        # print('s1', s1.get_vector())
+        # print('s2', s2.get_vector())
+        # print('s3', s3.get_vector())
 
 if __name__ == '__main__':
     unittest.main()
