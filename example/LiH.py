a8c072a71994a76f96cfddd3eeb2358c42d1b85e
Yuta Matsuzawa <matsuzawa@theoc.kuchem.kyoto-u.ac.jp>
2019-11-25 18:50:23 +0900

    [add] exampleを追加

---
+++ example/LiH.py
@@ -0,0 +1,11 @@
+from pyscf import gto
+import freqerica
+
+mol = gto.Mole()
+mol.basis = "sto-6g"
+mol.verbose = 0
+mol.spin = 0
+mol.atom = [["Li", (0.0, 0.0, 0.0)],["H", (0.0, 0.0, 1.5)]]
+mol.build()
+
+result = freqerica.kernel(mol, norb=5, nelec=2)
