b9078b8a463835e91923dbb04d9f5a9bfde932c3
Yuta Matsuzawa <matsuzawa@theoc.kuchem.kyoto-u.ac.jp>
2019-11-26 18:11:16 +0900

    [modify] mol.symmetry = True

--- example/LiH.py
+++ example/LiH.py
@@ -6,6 +6,7 @@ mol.basis = "sto-6g"
 mol.verbose = 0
 mol.spin = 0
 mol.atom = [["Li", (0.0, 0.0, 0.0)],["H", (0.0, 0.0, 1.5)]]
+mol.symmetry = True
 mol.build()
 
 result = freqerica.kernel(mol, norb=5, nelec=2)
