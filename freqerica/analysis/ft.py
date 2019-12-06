e9ab7a571cbba1865ce2d52cb756887e0190ea69
Yuta Matsuzawa <matsuzawa@theoc.kuchem.kyoto-u.ac.jp>
2019-12-07 07:56:03 +0900

    [modify] use_windowオプションを追加

--- freqerica/analysis/ft.py
+++ freqerica/analysis/ft.py
@@ -3,5 +3,7 @@ import numpy as np
 def window_func(time):
     return np.exp(-(time/time[-1]*2)**2)
 
-def ft(time, corr, energy_range):
-    return np.sum(np.exp(1j*time*energy_range.reshape(-1,1))*corr*window_func(time), axis=1)
+def ft(time, corr, energy_range, use_window=True):
+    if use_window:
+        return np.sum(np.exp(1j*time*energy_range.reshape(-1,1))*corr*window_func(time), axis=1)
+    return np.sum(np.exp(1j*time*energy_range.reshape(-1,1))*corr, axis=1)
